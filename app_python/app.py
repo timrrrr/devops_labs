3ac889b1a75b26143816ccc4147cd050a1d1ee84
timrrrr <54307723+timrrrr@users.noreply.github.com>
2022-12-13 00:26:16 +0300

    lab 14

--- app_python/app.py
+++ app_python/app.py
@@ -14,6 +14,21 @@ def time_page():
     return ('<h2 style="text-align:center;">' +
             datetime.now(timezone).strftime(TIME_FORMAT) + ' MSK<h2>')
 
+@app.get('/')
+def add_visit():
+    with open('visits.txt', 'a') as f:
+        f.write(time_controller.get_moscow_time() + '\n')
+
+@app.get('/visits')
+def get_visits():
+    with open('visits.txt', 'r') as f:
+        data = f.read()
+    
+    visit_times = data.split('\n')
+
+    return { 'visits': visit_times }
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8080, debug=True)
     
